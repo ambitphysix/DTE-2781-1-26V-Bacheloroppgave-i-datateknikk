d8a93af7ad9b05603d621fd72a67065df9280414
Tor Richard <tbo070@post.uit.no>
2026-03-24 23:01:52 +0100

    noen forskjellige index filer
    for å teste litt flere kart

--- backend/app/main/routes.py
+++ backend/app/main/routes.py
@@ -8,3 +8,10 @@ def index():
     Renders the index page.
     """
     return render_template('index.html')
+
+@main.route('/vis/<side>')
+def vis_side(side):
+    """
+    Renders the visualization page for a specific side.
+    """
+    return render_template(f'{side}.html')
