318f9a415b6c7c849f7222d6f60924415ffb82c4
Jens Petter <jth047@uit.no>
2026-05-11 11:33:41 +0200

    Fikset en bug ved sammenslåing av små polygoner

--- app/data/utils.py
+++ app/data/utils.py
@@ -3,10 +3,12 @@ import geopandas as gpd
 
 def parse_search_areas(search_areas: list[dict]) :
     gdf = gpd.GeoDataFrame.from_features(search_areas)
-    small_polygons = gdf[gdf.area < 0.02*10**6]
-    big_polygons = gdf[gdf.area >= 0.02*10**6].copy()
+    small_polygons = gdf[gdf.area < 0.04*10**6]
+    big_polygons = gdf[gdf.area >= 0.04*10**6].copy()
     for idx, poly in small_polygons.iterrows():
         edge_intersections = big_polygons.geometry.intersection(poly.geometry)
+        if edge_intersections.length.max() <= 0:
+            continue
         best_match_idx = edge_intersections.length.idxmax()
         big_polygons.at[best_match_idx, "geometry"] = big_polygons.at[best_match_idx, "geometry"].union(poly.geometry)
     return big_polygons.set_crs(epsg=25833).to_crs(epsg=4326).to_geo_dict()
